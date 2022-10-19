764f0eabef3244ba4d8f74eb70dd5c91178c5a84
bedoeva <115360343+bedoeva@users.noreply.github.com>
2022-10-19 12:21:36 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task 8.py
@@ -0,0 +1,13 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+month = 0
+
+while money_capital >= spend:
+    money_capital = money_capital + salary - spend
+    spend = spend + spend * increase
+    month += 1
+
+print(month)
